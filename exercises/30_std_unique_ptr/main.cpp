99669a56d1255eaca1d8723ca02e989a0e21a444
licy-123 <licy292556@gmail.com>
2026-01-02 18:12:31 +0800

    final2.0

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -35,7 +35,7 @@ Unique forward(Unique ptr) {
     return ptr;
 }
 
-/*
+
 int main(int argc, char **argv) {
     std::vector<std::string> problems[3];
 
@@ -55,7 +55,7 @@ int main(int argc, char **argv) {
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
         {"d", "ffr"},
-        {"r", "d", "d"},
+        {"d", "d", "r"},
     };
 
     // ---- 不要修改以下代码 ----
@@ -67,41 +67,5 @@ int main(int argc, char **argv) {
         }
     }
 
-    return 0;
-}
-*/
-// -------------- 调试版 main --------------
-int main(int argc, char **argv) {
-    std::vector<std::string> problems[3];
-
-    auto print = [&](int idx) {
-        std::cout << "chain " << idx << " : ";
-        for (auto &s : RECORDS) std::cout << '"' << s << "\" ";
-        std::cout << '\n';
-        problems[idx] = std::move(RECORDS);
-    };
-
-    drop(forward(reset(nullptr)));
-    print(0);
-
-    forward(drop(reset(forward(forward(reset(nullptr))))));
-    print(1);
-
-    drop(drop(reset(drop(reset(reset(nullptr))))));
-    print(2);
-
-    // 把下面终端里显示的字符串按顺序填进 answers 即可
-    std::vector<const char *> answers[] = {
-        {nullptr},   // 先空着，跑完按输出填
-        {nullptr},
-        {nullptr}
-    };
-
-    // 下面这段仅用于让你看到结果，不会进正式提交
-    for (int i = 0; i < 3; ++i) {
-        std::cout << "answers[" << i << "] = {";
-        for (auto &s : problems[i]) std::cout << "\"" << s << "\",";
-        std::cout << "}\n";
-    }
     return 0;
 }
